499dfc830cd98bf9056a697707ba319f0cba814a
jinjiantong <jinjiantong@yeah.net>
2024-09-26 12:13:32 +0800

    donload

--- src/App.js
+++ src/App.js
@@ -126,7 +126,7 @@ function App() {
                     }}
                     onClick={() => {
                       window.location.href =
-                        "https://file.chaogudr.com/tl1o9OG29Llrz3pTT9EqrVU8WlyruMqM/app-release.apk";
+                        "https://file.chaogudr.com/VKb0dMjSGWBQBjqgG7XrsK6RApSOrcpz/app-release.apk";
                     }}
                     icon={<IconPhone />}
                   >
